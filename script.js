f119354d45cd93678328d2f89bac100447581add
Gabriel Peixoto <peixoto.gabgab@outlook.com>
2024-08-02 23:29:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function contar() {
+    var ini = document.getElementById('txtinicio')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpasso')
+    var res = document.getElementById('res')
+
+   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+    res.innerHTML = 'Impossível contar!'
+    alert('Erro')
+   } else {
+    res.innerHTML = 'Contando: <br> '
+    var i = Number(ini.value)
+    var f = Number(fim.value)
+    var p = Number(passo.value)
+    if (p <= 0){
+        alert('Passo inválido! Considerando PASSO 1')
+        p = 1
+    }
+    if (i < f){
+        // Contagem crescente
+        for(var c = i; c<= f; c+=p){
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+    }else{
+        // Contagem regressiva
+        for(var c = i; c>= f; c-=p){
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+    }
+    res.innerHTML += `\u{1F3C1}`
+   }
+}
